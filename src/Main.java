be120e6e6c9091de35969ba2cbc0a9d7bf7f662e
thatimoreira <thatimoreira.dev@gmail.com>
2024-04-25 15:50:17 -0300

    Add GuessingGame project that implements a simple number guessing game. The game generates a  random number and the player has a limited number of attempts to guess it. After each guess, feedback is provided to help the player guess the number.

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+/*
+**  BRAZILIAN PORTUGUESE:
+**
+**      Jogo de Adivinhação
+**      Crie um programa que simula um jogo de adivinhação, que deve gerar um número aleatório entre 0 e 100
+**      e pedir para que o usuário tente adivinhar o número, em até 5 tentativas.
+**      A cada tentativa, o programa deve informar se o número digitado pelo usuário é maior ou menor do que o número gerado.
+**
+**  ENGLISH:
+**
+**      Guessing Game
+**      Create a program that simulates a guessing game, which should generate a random number between 0 and 100
+**      and ask the user to guess the number, until 5 attempts are made.
+**      At each attempt, the program should inform whether the number entered by the user is greater or less than the
+**      generated number.
+*/
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int         randomNumber;
+        int         userGuess;
+        int         attempts;
+        final int   MAX_ATTEMPTS;
+        Scanner     keyboard;
+
+        randomNumber = new Random().nextInt(100);
+        userGuess = 0;
+        attempts = 0;
+        MAX_ATTEMPTS = 5;
+        keyboard = new Scanner(System.in);
+        System.out.println("\n========== GUESS the NUMBER game ===========");
+        System.out.println("\nI have genarated a number between 0 and 100");
+        System.out.printf("You have %d attempts to guess it\n", MAX_ATTEMPTS);
+        //System.out.printf("Random number: %d\n", randomNumber);
+        while (userGuess != randomNumber && attempts < MAX_ATTEMPTS)
+        {
+            attempts++;
+            System.out.printf("\n------------- Attempt #%d -------------\n", attempts);
+            System.out.print("Guess a number between 0 and 100: ");
+            userGuess = keyboard.nextInt();
+            if (userGuess != randomNumber && attempts < MAX_ATTEMPTS)
+            {
+                if (userGuess > randomNumber)
+                {
+                    System.out.println("Your guess is greater than the number");
+                }
+                else
+                {
+                    System.out.println("Your guess is less than the number");
+                }
+                if (attempts == MAX_ATTEMPTS)
+                {
+                    System.out.println("You have reached the maximum number of attempts\n");
+                }
+            }
+            else
+            {
+                System.out.println("Congratulations! You guessed it!\n");
+            }
+        }
+        keyboard.close();
+        System.out.println("===========================================\n");
+    }
+}
